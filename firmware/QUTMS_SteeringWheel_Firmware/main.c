93e8a071933869facf3adce9197d815a5032669f
Zoe Goodward <zoe.j.goodward@gmail.com>
2019-09-03 12:58:37 +1000

    OLED functionality fully working, with "init" function that displays splash screen before entering normal operation

--- firmware/QUTMS_SteeringWheel_Firmware/main.c
+++ firmware/QUTMS_SteeringWheel_Firmware/main.c
@@ -18,15 +18,142 @@
 #include "images.h"
 #include "input.h"
 
+// Vertical scroll
+#define UPWARD 0x00
+#define DOWNWARD 0x01
+
+// Fade scroll
+#define UPWARD_OUT 0x02
+#define DOWNWARD_OUT 0x03
+
+
+/*============================================================================
+Function:   data_processing()
+------------------------------------------------------------------------------
+Purpose :   turns 1 byte of B/W data into 4 byte gray data
+Input   :   unsigned char temp -
+Returns :   return
+Notes   :
+============================================================================*/
+void data_processing(uint8_t temp)
+{
+	uint8_t temp1,temp2,temp3,temp4,temp5,temp6,temp7,temp8;
+	uint8_t h11,h12,h13,h14,h15,h16,h17,h18,d1,d2,d3,d4;
+
+	temp1=temp&0x80;
+	temp2=(temp&0x40)>>3;
+	temp3=(temp&0x20)<<2;
+	temp4=(temp&0x10)>>1;
+	temp5=(temp&0x08)<<4;
+	temp6=(temp&0x04)<<1;
+	temp7=(temp&0x02)<<6;
+	temp8=(temp&0x01)<<3;
+	h11=temp1|temp1>>1|temp1>>2|temp1>>3;
+	h12=temp2|temp2>>1|temp2>>2|temp2>>3;
+	h13=temp3|temp3>>1|temp3>>2|temp3>>3;
+	h14=temp4|temp4>>1|temp4>>2|temp4>>3;
+	h15=temp5|temp5>>1|temp5>>2|temp5>>3;
+	h16=temp6|temp6>>1|temp6>>2|temp6>>3;
+	h17=temp7|temp7>>1|temp7>>2|temp7>>3;
+	h18=temp8|temp8>>1|temp8>>2|temp8>>3;
+	d1=h11|h12;
+	d2=h13|h14;
+	d3=h15|h16;
+	d4=h17|h18;
+
+	oled_write_data(d1);
+	oled_write_data(d2);
+	oled_write_data(d3);
+	oled_write_data(d4);
+}
+
+/*============================================================================
+Function:   display_picture()
+------------------------------------------------------------------------------
+Purpose :   displays a bitmap image on the OLED screen
+Input   :   unsigned char pic[] - an array of hexidecimal values
+Returns :   return
+Notes   :
+============================================================================*/
+void display_picture(const unsigned char pic[])
+{
+	uint8_t i,j;
+	set_column_address(Shift+0x00,Shift+0x77);
+	set_row_address(0x00,0x7F);
+	set_write_RAM();
+
+	for(i=0;i<64;i++)
+	{
+		for(j=0;j<60;j++)
+		{
+			data_processing(pgm_read_byte(&pic[i*32+j]));
+		}
+	}
+	return;
+}
+
+/*============================================================================
+Function:   splash_screen()
+------------------------------------------------------------------------------
+Purpose :   displays the QUT Motorsport logo when the steering wheel system
+			is first initialised / alternatively can display party parrot
+Input   :   none
+Returns :   void
+Notes   :
+============================================================================*/
+void splash_screen()
+{
+	fill_RAM(CLEAR_SCREEN);
+	display_picture(MOTORSPORT);
+	_delay_ms(3000);
+	fill_RAM(CLEAR_SCREEN);
+	// Add transition effect
+}
+
+/*============================================================================
+Function:   steering_wheel_init()
+------------------------------------------------------------------------------
+Purpose :   consolidates all the functions required for the initialisation of
+			the steering wheel
+Input   :   none
+Returns :   void
+Notes   :
+============================================================================*/
+void steering_wheel_init() 
+{
+	DDRD |= 0b11001000; // LEDs as outputs, DC
+	/* Set MOSI and SCK output, all others input */
+	DDRB |= 0b10100011; // CS/SS, reset
+	// TXD/MOSI_A, OLED_CS	
+	SPI_CLOCK_HIGH;
+	//spi_init(SPI_INTERRUPT_DISABLED, MSBFIRST, SPI_MASTER, SPI_MODE_0, SPI_CLOCK_RATE_0);
+	//SPI_CLOCK_HIGH;
+	spi_init();
+	OLED_init();	
+	sei(); // Enable interrupts
+	splash_screen();
+}
+
+
 int main(void)
 {
+	steering_wheel_init();
+
 	while (1)
 	{
 		LED_A_ON;
 		LED_B_OFF;
-		_delay_ms(100);		
+
+		oled_write_data(0b10000000);
+		//_delay_ms(500);
+		
+		oled_write_data(0b00000001);
+		_delay_ms(100);
+		
 		LED_A_OFF;
 		LED_B_ON;
+		
+		oled_write_data(0b00100000);
 		_delay_ms(500);
 	}
 }
